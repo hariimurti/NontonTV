2f3d612b379e1e4e9e67872199ee65cdcb8723fb
Harimurti <hariimurti@gmail.com>
2020-04-09 00:11:01 +0700

    main: rework layout spin/message

--- app/src/main/java/net/harimurti/tv/MainActivity.java
+++ app/src/main/java/net/harimurti/tv/MainActivity.java
@@ -6,19 +6,23 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import com.github.ybq.android.spinkit.SpinKitView;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
 import net.harimurti.tv.adapter.ViewPagerAdapter;
 import net.harimurti.tv.data.Playlist;
+import net.harimurti.tv.extra.AsyncSleep;
+import net.harimurti.tv.extra.Network;
 import net.harimurti.tv.extra.RestClient;
 import net.harimurti.tv.extra.RestClient.OnClientResult;
 
 public class MainActivity extends AppCompatActivity {
+    private RestClient client;
+    private View layoutStatus, layoutSpin, layoutText;
+    private TextView tvStatus, tvRetry;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,29 +32,30 @@ public class MainActivity extends AppCompatActivity {
         // define some view
         TabLayout tabLayout = findViewById(R.id.tab_layout);
         ViewPager2 viewPager = findViewById(R.id.view_pager);
-        RelativeLayout layoutMessage = findViewById(R.id.layout_status);
-        SpinKitView spinKit = findViewById(R.id.spin_kit);
-        TextView textMessage = findViewById(R.id.text_status);
+        layoutStatus = findViewById(R.id.layout_status);
+        layoutSpin = findViewById(R.id.layout_spin);
+        layoutText = findViewById(R.id.layout_text);
+        tvStatus = findViewById(R.id.text_status);
+        tvRetry = findViewById(R.id.text_retry);
 
         // create client & set listener -> getchannels
-        RestClient client = new RestClient(this);
+        client = new RestClient(this);
         client.setOnClientResult(new OnClientResult() {
             @Override
             public void onFailure(String status) {
                 runOnUiThread(() -> {
-                    layoutMessage.setVisibility(View.VISIBLE);
-                    spinKit.setVisibility(View.INVISIBLE);
-                    textMessage.setVisibility(View.VISIBLE);
-                    textMessage.setText(status);
+                    ShowLayoutMessage(View.VISIBLE, true);
+                    tvStatus.setText(status);
+
+                    // try fetching later
+                    RetryGetChannels();
                 });
             }
 
             @Override
             public void onProgress(boolean status) {
                 runOnUiThread(() -> {
-                    layoutMessage.setVisibility(status ? View.VISIBLE : View.GONE);
-                    spinKit.setVisibility(View.VISIBLE);
-                    textMessage.setVisibility(View.GONE);
+                    ShowLayoutMessage(status ? View.VISIBLE : View.GONE, false);
                 });
             }
 
@@ -63,7 +68,39 @@ public class MainActivity extends AppCompatActivity {
                     ).attach();
                 });
             }
-        }).GetChannels();
+        });
+        client.GetChannels();
+    }
+
+    private void RetryGetChannels() {
+        Network network = new Network(this);
+        new AsyncSleep(this).task(new AsyncSleep.Task() {
+            @Override
+            public void onCountDown(int left) {
+                if (!network.IsConnected())
+                    tvStatus.setText(getString(R.string.no_network));
+
+                tvRetry.setText(String.format(getString(R.string.retry_time), left));
+            }
+            @Override
+            public void onFinish() {
+                if (network.IsConnected())
+                    client.GetChannels();
+                else
+                    RetryGetChannels();
+            }
+        }).start(5);
+    }
+
+    private void ShowLayoutMessage(int visibility, boolean isMessage) {
+        layoutStatus.setVisibility(visibility);
+        if (!isMessage) {
+            layoutSpin.setVisibility(View.VISIBLE);
+            layoutText.setVisibility(View.GONE);
+        } else {
+            layoutSpin.setVisibility(View.GONE);
+            layoutText.setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
