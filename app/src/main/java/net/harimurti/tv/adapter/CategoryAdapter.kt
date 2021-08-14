1e60a68ae2f4a4b5f42d3e8e09a68502cdca636d
Harimurti <hariimurti@gmail.com>
2021-08-15 02:52:20 +0700

    main: match span grid with total channels

--- app/src/main/java/net/harimurti/tv/adapter/CategoryAdapter.kt
+++ app/src/main/java/net/harimurti/tv/adapter/CategoryAdapter.kt
@@ -5,10 +5,12 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import net.harimurti.tv.BR
 import net.harimurti.tv.R
 import net.harimurti.tv.databinding.ItemCategoryBinding
 import net.harimurti.tv.model.Category
+import kotlin.math.round
 
 class CategoryAdapter (cat: ArrayList<Category>?) :
     RecyclerView.Adapter<CategoryAdapter.ViewHolder>() {
@@ -32,7 +34,17 @@ class CategoryAdapter (cat: ArrayList<Category>?) :
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
         val category: Category? = categories?.get(position)
+        val chCount = category?.channels?.size ?: 0
+        val spanCount = when {
+            chCount > 30 -> chCount.toDouble().div(20).let { round(it).toInt() }
+            chCount > 20 -> 2
+            else -> 1
+        }
+
         viewHolder.itemCatBinding.chAdapter = ChannelAdapter(category?.channels)
+        viewHolder.itemCatBinding.rvChannels.layoutManager =
+                StaggeredGridLayoutManager(spanCount, StaggeredGridLayoutManager.HORIZONTAL)
+
         viewHolder.bind(category)
     }
 
