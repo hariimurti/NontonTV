2c18e903e5ab18257eb18c33c481dd2e176bdabf
Harimurti <hariimurti@gmail.com>
2021-09-03 19:16:30 +0700

    setting: add apk version in about page

--- app/src/main/java/net/harimurti/tv/dialog/SettingAboutFragment.kt
+++ app/src/main/java/net/harimurti/tv/dialog/SettingAboutFragment.kt
@@ -1,5 +1,6 @@
 package net.harimurti.tv.dialog
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -7,14 +8,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import net.harimurti.tv.BuildConfig
 import net.harimurti.tv.R
 import net.harimurti.tv.databinding.SettingAboutFragmentBinding
 import net.harimurti.tv.extra.Preferences
 
 class SettingAboutFragment: Fragment() {
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val binding = SettingAboutFragmentBinding.inflate(inflater, container, false)
-
+        binding.textVersion.text = "NontonTV v${BuildConfig.VERSION_NAME} b${BuildConfig.VERSION_CODE}"
         binding.textUsers.text = Preferences().contributors
         binding.buttonWebsite.setOnClickListener {
             openWebsite(getString(R.string.website))
