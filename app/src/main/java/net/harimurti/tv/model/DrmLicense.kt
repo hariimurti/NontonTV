ba15dbe415592eee8a096a22482bb1a544cda17d
Harimurti Wibowo <hariimurti@gmail.com>
2021-09-27 23:55:16 +0700

    app: lateinit drm value

--- app/src/main/java/net/harimurti/tv/model/DrmLicense.kt
+++ app/src/main/java/net/harimurti/tv/model/DrmLicense.kt
@@ -6,7 +6,7 @@ class DrmLicense {
     @SerializedName("drm_id")
     var id: String? = null
     @SerializedName("drm_type")
-    var type: String = ""
+    lateinit var type: String
     @SerializedName("drm_key")
-    var key: String = ""
+    lateinit var key: String
 }
